7e0866bacaea4af48f9d563900a5b1e85ace5ddb
Daniel Cripps <binary.riviera@gmail.com>
2024-10-21 17:50:00 +0100

    add test for multiple put and gets

--- tests/test_bitcask.py
+++ tests/test_bitcask.py
@@ -32,5 +32,24 @@ class TestBitcask(unittest.TestCase):
         # Get the key value pair out again
         self.assertEqual(bitcask.get(key), value)
 
+    @patch('uuid.uuid4')
+    def test_multiple_insert(self, mock_uuid):
+        mock_uuid.return_value = 'abc123xyz789'
+        bitcask = Bitcask()
+        bitcask.open(DB_PATH)
+
+        key1 = b'key1'
+        value1 = b'value1'
+        key2 = b'key2'
+        value2 = b'value2'
+
+        bitcask.put(key1, value1)
+        bitcask.put(key2, value2)
+        self.assertEqual(len(bitcask.keydir), 2)
+        self.assertEqual(bitcask.get(key1), value1)
+        self.assertEqual(bitcask.get(key2), value2)
+        
+        
+
         
 
